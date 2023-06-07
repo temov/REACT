6542499db9df6ab99e6754fea1b7adeb705381da
Tode Temov <75773817+temov@users.noreply.github.com>
2023-06-07 16:27:15 +0200

    Add files via upload

---
+++ movie_library_app-/src/App.js
@@ -0,0 +1,30 @@
+
+import './App.css';
+import moviesList from './components/MoviesList/movieslist.js';
+
+function App() {
+
+  const fullName = 'Tode Temov';
+  const printMessage = (text) => {
+    return text
+  };
+  return (
+    <div className="App">
+      <h1>Hello, my name is:{fullName}, and this is my first React app</h1>
+      <hr/>
+
+        <div className="container">
+          <h3>Today date is: {new Date().toDateString()}</h3>
+          <p>{printMessage('Below you have my 10 most popular movies')}</p>
+        
+        <hr/>
+
+        
+          <moviesList />
+        </div>
+      
+    </div>
+  );
+}
+
+export default App;
